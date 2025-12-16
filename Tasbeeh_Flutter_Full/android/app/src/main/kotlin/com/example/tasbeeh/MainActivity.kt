23508b80ba5e79b862a98b4d5de2007a5faec7d5
ahmedgabertaeseer-cpu <ahmed.gaber.taeseer@gmail.com>
2025-12-16 11:39:23 +0200

    Add files via upload

---
+++ Tasbeeh_Flutter_Full/android/app/src/main/kotlin/com/example/tasbeeh/MainActivity.kt
@@ -0,0 +1,7 @@
+
+package com.example.tasbeeh
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ Tasbeeh_Flutter_Full/lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const TasbeehApp());
+}
+
+class TasbeehApp extends StatelessWidget {
+  const TasbeehApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: Center(
+          child: Text('Tasbeeh App', style: TextStyle(fontSize: 24)),
+        ),
+      ),
+    );
+  }
+}
